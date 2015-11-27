8227b153b0cfc0856f24f56d456ca6601a232ac3
Rasmus Kaj <kaj@kth.se>
2015-11-27 13:05:43 +0100

    Put RustormMiddleware in separate file.

--- src/main.rs
+++ src/main.rs
@@ -14,59 +14,20 @@ use hyper::header::{Expires, HttpDate};
 use image::open as image_open;
 use image::{FilterType, ImageFormat};
 use models::{Photo, Tag, query_for};
-use nickel::{Continue, MediaType, Middleware, MiddlewareResult, Nickel,
-             Request, Response, StaticFilesHandler};
-use plugin::{Pluggable, Extensible};
+use nickel::{MediaType, Nickel, StaticFilesHandler};
+use plugin::{Pluggable};
 use rustc_serialize::Encodable;
-use rustorm::dao::{IsDao, ToValue};
-use rustorm::database::{Database, DbError};
-use rustorm::pool::{ManagedPool,Platform};
+use rustorm::database::{Database};
 use rustorm::query::Query;
 use rustorm::table::IsTable;
 use std::collections::HashMap;
 use time::Duration;
-use typemap::Key;
 
 mod env;
 use env::dburl;
 
-struct RustormMiddleware {
-    pool: ManagedPool
-}
-
-impl RustormMiddleware {
-    pub fn new(db_url: &str) -> RustormMiddleware {
-        RustormMiddleware {
-            pool: ManagedPool::init(db_url, 5).unwrap(),
-        }
-    }
-}
-
-impl Key for RustormMiddleware { type Value = Platform; }
-
-impl<D> Middleware<D> for RustormMiddleware {
-    fn invoke<'mw, 'conn>(&self, req: &mut Request<'mw, 'conn, D>, res: Response<'mw, D>) -> MiddlewareResult<'mw, D> {
-        req.extensions_mut().insert::<RustormMiddleware>(
-            self.pool.connect().unwrap());
-        Ok(Continue(res))
-    }
-}
-
-pub trait RustormRequestExtensions {
-    fn db_conn(&self) -> &Database;
-    fn orm_get<T: IsTable + IsDao>(&self, key: &str, val: &ToValue)
-                                   -> Result<T, DbError>;
-}
-
-impl<'a, 'b, D> RustormRequestExtensions for Request<'a, 'b, D> {
-    fn db_conn(&self) -> &Database {
-        self.extensions().get::<RustormMiddleware>().unwrap().as_ref()
-    }
-    fn orm_get<T: IsTable + IsDao>(&self, key: &str, val: &ToValue)
-                                   -> Result<T, DbError> {
-        query_for::<T>().filter_eq(key, val).collect_one(self.db_conn())
-    }
-}
+mod rustormmiddleware;
+use rustormmiddleware::{RustormMiddleware, RustormRequestExtensions};
 
 #[derive(Debug, Clone, RustcEncodable)]
 struct DetailsData {
