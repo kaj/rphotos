93c7d4fbbbe7ace85591bd3b5b1c2298e3d9a43b
Rasmus Kaj <kaj@kth.se>
2016-05-21 17:56:48 +0200

    Some clean-up of sassify.

--- src/sassify.rs
+++ src/sassify.rs
@@ -5,51 +5,61 @@ extern crate rustc_serialize as serialize;
 
 use sass_rs::dispatcher::Dispatcher;
 use sass_rs::sass_context::SassFileContext;
-use sass_rs::sass_function::*;
 use serialize::base64::{self, ToBase64};
 use std::env;
 use std::fs::{File, create_dir_all};
 use std::io::Write;
-use std::path::Path;
+use std::path::PathBuf;
 use std::thread;
 
 fn main() {
-    let rod = env::var("OUT_DIR").unwrap();
-    let od = Path::new(&rod);
-    let css = compile("photos.scss").unwrap();
-    let filename = format!("style-{}.css",
-                           md5::compute(css.as_bytes())[9..]
-                           .to_base64(base64::URL_SAFE));
-    let staticdir = od.join("static").join("static");
-    create_dir_all(&staticdir).unwrap();
-    let mut f = &File::create(&staticdir.join(&*filename)).unwrap();
-    write!(f, "{}", css).unwrap();
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let static_dir = out_dir.join("static").join("static");
+    create_dir_all(&static_dir).unwrap();
 
-    let mut link = &File::create(&od.join("stylelink")).unwrap();
-    writeln!(link,
-             "\"<link rel='stylesheet' href='/static/{}' type='text/css'/>\"",
-             filename).unwrap();
+    let css = compile("photos.scss").unwrap();
+    let filename = format!("style-{}.css", checksum_slug(&css));
+
+    File::create(&static_dir.join(&filename))
+        .map(|mut f| {
+            write!(f, "{}", css).unwrap();
+        })
+        .unwrap();
+
+    File::create(&out_dir.join("stylelink"))
+        .map(|mut f| {
+            writeln!(f,
+                     "\"<link rel='stylesheet' href='/static/{}' \
+                      type='text/css'/>\"",
+                     filename)
+                .unwrap();
+        })
+        .unwrap();
 
     println!("cargo:rerun-if-changed=src/sassify.rs");
     // TODO Find any referenced files!
     println!("cargo:rerun-if-changed=photos.scss");
 }
 
-/// Setup the environment and compile a file.
-fn compile(filename:&str) -> Result<String, String> {
+/// A short and url-safe checksum string from string data.
+fn checksum_slug(data: &str) -> String {
+    md5::compute(data.as_bytes())[9..].to_base64(base64::URL_SAFE)
+}
+
+/// Setup the sass environment and compile a file.
+fn compile(filename: &str) -> Result<String, String> {
     let mut file_context = SassFileContext::new(filename);
-    let fns:Vec<(&'static str,Box<SassFunction>)> = vec![];
-    let options = file_context.sass_context.sass_options.clone();
     // options.set_output_style(COMPRESSED) or similar, when supported.
-    if let Ok(mut opt) = (*options).write() {
+    if let Ok(mut opt) = file_context.sass_context.sass_options.write() {
         unsafe {
             sass_sys::sass_option_set_output_style(
                 opt.raw.get_mut(),
                 sass_sys::SASS_STYLE_COMPRESSED);
         }
     }
-    thread::spawn(move|| {
-        let dispatcher = Dispatcher::build(fns, options);
+    let options = file_context.sass_context.sass_options.clone();
+    thread::spawn(move || {
+        let dispatcher = Dispatcher::build(vec![], options);
         while dispatcher.dispatch().is_ok() {}
     });
     file_context.compile()
