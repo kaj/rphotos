b1a8360e5c6059b38436673d05da6de60f4f95f8
Rasmus Kaj <kaj@kth.se>
2017-12-07 07:17:09 +0100

    Use flate2 1.x.
    There was a slight API change.

--- src/adm/storestatics.rs
+++ src/adm/storestatics.rs
@@ -1,6 +1,6 @@
 use adm::result::Error;
 use brotli2::write::BrotliEncoder;
-use flate2::{Compression, FlateWriteExt};
+use flate2::{Compression, GzBuilder};
 use std::fs::{create_dir_all, File};
 use std::io::prelude::*;
 use std::path::Path;
@@ -13,8 +13,11 @@ pub fn to_dir(dir: &str) -> Result<(), Error> {
         File::create(dir.join(s.name)).and_then(|mut f| f.write(s.content))?;
 
         File::create(dir.join(format!("{}.gz", s.name)))
-            .map(|f| f.gz_encode(Compression::Best))
-            .and_then(|mut f| f.write(s.content))?;
+            .map(|f| GzBuilder::new().write(f, Compression::best()))
+            .and_then(|mut gz| {
+                gz.write(s.content)?;
+                gz.finish()
+            })?;
 
         File::create(dir.join(format!("{}.br", s.name)))
             .map(|f| BrotliEncoder::new(f, 11))
