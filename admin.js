1c6ed1c70909574c85e6d4bc08c37e51a211154d
Rasmus Kaj <kaj@kth.se>
2017-12-11 23:11:36 +0100

    Shorten and simplify admin.js.

--- admin.js
+++ admin.js
@@ -1,29 +1,29 @@
 // Admin functionality for rphotos
-function rpadmin() {
-    var details = document.querySelector('.details');
+(function (d) {
+    var details = d.querySelector('.details');
 
     function rotate(event) {
         var imgid = details.dataset.imgid;
         var angle = event.target.dataset.angle;
         var r = new XMLHttpRequest();
-        document.body.classList.add('busy');
+        d.body.classList.add('busy');
         r.open('POST', '/adm/rotate');
         r.onload = function() {
             if (r.status === 200) {
                 // Yay!
-                document.location.reload(true);
+                d.location.reload(true);
             } else {
                 alert("Rotating failed: " + r.status);
             }
-            document.body.classList.remove('busy');
+            d.body.classList.remove('busy');
         }
         r.onerror = function() {
             alert("Rotating failed.");
-            document.body.classList.remove('busy');
+            d.body.classList.remove('busy');
         }
         r.ontimeout = function() {
             alert("Rotating timed out");
-            document.body.classList.remove('busy');
+            d.body.classList.remove('busy');
         }
         r.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         r.send("angle=" + angle + "&image=" + imgid)
@@ -32,26 +32,26 @@ function rpadmin() {
     function tag_form(event, category) {
         event.target.disabled = true;
         var imgid = details.dataset.imgid;
-        var f = document.createElement("form");
+        var f = d.createElement("form");
         f.className = "admin " + category;
         f.action = "/adm/" + category;
         f.method = "post";
-        var l = document.createElement("label");
+        var l = d.createElement("label");
         l.innerHTML = event.target.title;
         f.appendChild(l);
-        var i = document.createElement("input");
+        var i = d.createElement("input");
         i.type="hidden";
         i.name="image";
         i.value = imgid;
         f.appendChild(i);
-        i = document.createElement("input");
+        i = d.createElement("input");
         i.type = "text";
         i.autocomplete="off";
         i.tabindex="1";
         i.name = category;
         i.id = category+'name';
         l.htmlFor = i.id;
-        var list = document.createElement("div");
+        var list = d.createElement("div");
         list.className = "completions";
         i.addEventListener('keyup', e => {
             let c = e.code;
@@ -67,7 +67,7 @@ function rpadmin() {
                     list.style.left = i.offsetLeft + "px"
                     list.innerHTML = '';
                     t.map(x => {
-			let a = document.createElement('a');
+			let a = d.createElement('a');
 			a.innerHTML = x;
 			a.tabIndex = 2;
 			a.href = "#";
@@ -82,7 +82,7 @@ function rpadmin() {
 			list.appendChild(a)
                     })
 		};
-		r.open('GET', document.location.origin + '/ac/' + category + '?q=' + encodeURIComponent(v));
+		r.open('GET', d.location.origin + '/ac/' + category + '?q=' + encodeURIComponent(v));
 		r.send(null);
             } else {
 		list.innerHTML = '';
@@ -119,11 +119,11 @@ function rpadmin() {
             e.stopPropagation();
             return false;
         });
-        let s = document.createElement("button");
+        let s = d.createElement("button");
         s.innerHTML = "Ok";
         s.type = "submit";
         f.appendChild(s);
-        let c = document.createElement("button");
+        let c = d.createElement("button");
         c.innerHTML = "&#x1f5d9;";
         c.className = 'close';
         c.title = 'close';
@@ -141,19 +141,19 @@ function rpadmin() {
         event.target.disabled = true;
         var imgid = details.dataset.imgid;
         var grade = details.dataset.grade;
-        var f = document.createElement("form");
+        var f = d.createElement("form");
         f.className = "admin grade";
         f.action = "/adm/grade";
         f.method = "post";
-        var l = document.createElement("label");
+        var l = d.createElement("label");
         l.innerHTML = event.target.title;
         f.appendChild(l);
-        var i = document.createElement("input");
+        var i = d.createElement("input");
         i.type="hidden";
         i.name="image";
         i.value = imgid;
         f.appendChild(i);
-        i = document.createElement("input");
+        i = d.createElement("input");
         i.type="range";
         i.name="grade";
         if (grade) {
@@ -162,11 +162,11 @@ function rpadmin() {
         i.min=0;
         i.max=100;
         f.appendChild(i);
-        let s = document.createElement("button");
+        let s = d.createElement("button");
         s.innerHTML = "Ok";
         s.type = "submit";
         f.appendChild(s);
-        let c = document.createElement("button");
+        let c = d.createElement("button");
         c.innerHTML = "&#x1f5d9;";
         c.className = 'close';
         c.title = 'close';
@@ -199,39 +199,39 @@ function rpadmin() {
 
     var meta = details.querySelector('.meta');
     if (meta) {
-        p = document.createElement("p");
-        r = document.createElement("button");
+        p = d.createElement("p");
+        r = d.createElement("button");
         r.onclick = rotate;
         r.innerHTML = "\u27f2";
         r.dataset.angle = "-90";
         r.title = "Rotate left";
         p.appendChild(r);
-        p.appendChild(document.createTextNode(" "));
-        r = document.createElement("button");
+        p.appendChild(d.createTextNode(" "));
+        r = d.createElement("button");
         r.onclick = rotate;
         r.innerHTML = "\u27f3";
         r.dataset.angle = "90";
         r.title = "Rotate right";
         p.appendChild(r);
 
-        p.appendChild(document.createTextNode(" "));
-        r = document.createElement("button");
+        p.appendChild(d.createTextNode(" "));
+        r = d.createElement("button");
         r.onclick = e => tag_form(e, 'tag');
         r.innerHTML = "&#x1f3f7;";
         r.title = "Tag";
         r.accessKey = "t";
         p.appendChild(r);
 
-        p.appendChild(document.createTextNode(" "));
-        r = document.createElement("button");
+        p.appendChild(d.createTextNode(" "));
+        r = d.createElement("button");
         r.onclick = e => tag_form(e, 'person');
         r.innerHTML = "\u263a";
         r.title = "Person";
         r.accessKey = "p";
         p.appendChild(r);
 
-        p.appendChild(document.createTextNode(" "));
-        r = document.createElement("button");
+        p.appendChild(d.createTextNode(" "));
+        r = d.createElement("button");
         r.onclick = e => grade_form(e);
         r.innerHTML = "\u2606";
         r.title = "Grade";
@@ -239,4 +239,4 @@ function rpadmin() {
         p.appendChild(r);
         meta.appendChild(p);
     }
-}
+})(document)
