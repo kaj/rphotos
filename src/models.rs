39126f2bc03815d64b4bc3103a22b98dfb7c3756
Rasmus Kaj <kaj@kth.se>
2018-09-06 13:50:34 +0200

    Improve presentation of places on a photo.

--- src/models.rs
+++ src/models.rs
@@ -150,11 +150,12 @@ impl Photo {
         db: &PgConnection,
     ) -> Result<Vec<Place>, DieselError> {
         use schema::photo_places::dsl::{photo_id, photo_places, place_id};
-        use schema::places::dsl::{id, places};
+        use schema::places::dsl::{id, osm_level, places};
         places
             .filter(id.eq_any(
                 photo_places.select(place_id).filter(photo_id.eq(self.id)),
-            )).load(db)
+            )).order(osm_level.desc().nulls_first())
+            .load(db)
     }
     pub fn load_tags(
         &self,
