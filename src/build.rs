7e1c79fa8b425fc8e47d24638d109efed9a3303c
Rasmus Kaj <kaj@kth.se>
2019-04-20 22:04:09 +0200

    Update ructe.

--- src/build.rs
+++ src/build.rs
@@ -1,22 +1,13 @@
-extern crate ructe;
+use ructe::{Ructe, RucteError};
 
-use ructe::{compile_templates, StaticFiles};
-use std::env;
-use std::path::PathBuf;
-
-fn main() {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let base_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    let mut statics = StaticFiles::new(&out_dir).unwrap();
-    let s_dir = base_dir.join("res");
-    statics.add_sass_file(&s_dir.join("photos.scss")).unwrap();
-    statics.add_file(&s_dir.join("admin.js")).unwrap();
-    statics.add_file(&s_dir.join("ux.js")).unwrap();
-    statics
-        .add_files_as(&s_dir.join("leaflet-1.3.1"), "l131")
-        .unwrap();
-    statics
-        .add_files_as(&s_dir.join("leaflet-cluster-1.3.0"), "lm130")
-        .unwrap();
-    compile_templates(&base_dir.join("templates"), &out_dir).unwrap();
+fn main() -> Result<(), RucteError> {
+    let mut ructe = Ructe::from_env()?;
+    let mut statics = ructe.statics()?;
+    statics.add_sass_file("res/photos.scss")?;
+    statics.add_file("res/admin.js")?;
+    statics.add_file("res/ux.js")?;
+    statics.add_files_as("res/leaflet-1.3.1", "l131")?;
+    statics.add_files_as("res/leaflet-cluster-1.3.0", "lm130")?;
+    ructe.compile_templates("templates")?;
+    Ok(())
 }
