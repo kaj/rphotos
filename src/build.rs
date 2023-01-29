02564ed6fc27aef98b62036b4e04bad8b424f529
Rasmus Kaj <kaj@kth.se>
2023-01-29 01:12:16 +0100

    Add first attempt of a favicon.

--- src/build.rs
+++ src/build.rs
@@ -4,6 +4,7 @@ fn main() -> Result<(), RucteError> {
     let mut ructe = Ructe::from_env()?;
     let mut statics = ructe.statics()?;
     statics.add_sass_file("res/photos.scss")?;
+    statics.add_file("res/rphotos.svg")?;
     statics.add_file("res/admin.js")?;
     statics.add_file("res/ux.js")?;
     statics.add_files_as("res/leaflet-1.4.0", "l140")?;
